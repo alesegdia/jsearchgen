a2ba150c70ee6a99b7b94f12932aa7685f450781
Alejandro Seguí <alesegdia@gmail.com>
2015-04-07 22:23:39 +0200

    Adding room list prefab.

--- project/src/com/alesegdia/jsearchgen/map/Prefabs.java
+++ project/src/com/alesegdia/jsearchgen/map/Prefabs.java
@@ -12,12 +12,17 @@ public final class Prefabs {
 	
 	public static List<RoomInstance> GenerateSampleRoomList()
 	{
-		RoomInstance room1 = new RoomInstance(Prefabs.room0);
+		RoomInstance room1 = new RoomInstance(Prefabs.room1);
 		RoomInstance room2 = new RoomInstance(Prefabs.room0);
 		RoomInstance room3 = new RoomInstance(Prefabs.room1);
-		room1.GenerateRandomDoors(RNG.rng, 3);
-		room2.GenerateRandomDoors(RNG.rng, 2);
-		room3.GenerateRandomDoors(RNG.rng, 2);
+		room1.GenerateRandomDoors(RNG.rng, 10);
+		room2.GenerateRandomDoors(RNG.rng, 10);
+		room3.GenerateRandomDoors(RNG.rng, 10);
+		room1.name = "room1";
+		room2.name = "room2";
+		room3.name = "room3";
+		
+		room1.RenderCanvas();
 		List<RoomInstance> initial_rooms = new LinkedList<RoomInstance>();
 		initial_rooms.add(room1);
 		initial_rooms.add(room2);
