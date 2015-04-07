416a37811f197ad42c419c505a841062b131cb89
Alejandro Seguí <alesegdia@gmail.com>
2015-04-07 19:56:06 +0200

    Adding sample room list to Prefab.

--- project/src/com/alesegdia/jsearchgen/map/Prefabs.java
+++ project/src/com/alesegdia/jsearchgen/map/Prefabs.java
@@ -1,8 +1,27 @@
 package com.alesegdia.jsearchgen.map;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.alesegdia.jsearchgen.util.RNG;
+
 public final class Prefabs {
 
 	public static RoomPrefab room0 = new RoomPrefab(new TileMap("rooms/room0.json"));
 	public static RoomPrefab room1 = new RoomPrefab(new TileMap("rooms/room1.json"));
 	
+	public static List<RoomInstance> GenerateSampleRoomList()
+	{
+		RoomInstance room1 = new RoomInstance(Prefabs.room0);
+		RoomInstance room2 = new RoomInstance(Prefabs.room0);
+		RoomInstance room3 = new RoomInstance(Prefabs.room1);
+		room1.GenerateRandomDoors(RNG.rng, 3);
+		room2.GenerateRandomDoors(RNG.rng, 2);
+		room3.GenerateRandomDoors(RNG.rng, 2);
+		List<RoomInstance> initial_rooms = new LinkedList<RoomInstance>();
+		initial_rooms.add(room1);
+		initial_rooms.add(room2);
+		initial_rooms.add(room3);
+		return initial_rooms;
+	}
 }
