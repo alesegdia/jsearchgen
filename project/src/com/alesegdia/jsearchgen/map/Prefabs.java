428843d9819f1fb3a76a1e4cf8e9b43ce7a87514
Alejandro Seguí <alesegdia@gmail.com>
2015-04-22 22:08:08 +0200

    Generating debug prefabs.

--- project/src/com/alesegdia/jsearchgen/map/Prefabs.java
+++ project/src/com/alesegdia/jsearchgen/map/Prefabs.java
@@ -3,6 +3,7 @@ package com.alesegdia.jsearchgen.map;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.alesegdia.jsearchgen.map.Door.Type;
 import com.alesegdia.jsearchgen.util.RNG;
 
 public final class Prefabs {
@@ -16,13 +17,13 @@ public final class Prefabs {
 		RoomInstance room2 = new RoomInstance(Prefabs.room0);
 		RoomInstance room3 = new RoomInstance(Prefabs.room1);
 		//RNG.rng.setSeed(234252);
-		room1.GenerateRandomDoors(RNG.rng, 10);
-		room2.GenerateRandomDoors(RNG.rng, 10);
-		room3.GenerateRandomDoors(RNG.rng, 10);
+		room1.GenerateRandomDoors(RNG.rng, 2);
+		room2.GenerateRandomDoors(RNG.rng, 2);
+		room3.GenerateRandomDoors(RNG.rng, 2);
 		room1.name = "room1";
 		room2.name = "room2";
 		room3.name = "room3";
-fdf
+
 		room1.RenderCanvas();
 		room2.RenderCanvas();
 		room3.RenderCanvas();
@@ -32,4 +33,24 @@ fdf
 		initial_rooms.add(room3);
 		return initial_rooms;
 	}
+	
+	public static List<RoomInstance> GenerateFixedDoorRoomList()
+	{
+		RoomInstance room1 = new RoomInstance(Prefabs.room0);
+		RoomInstance room2 = new RoomInstance(Prefabs.room1);
+		//RNG.rng.setSeed(234252);
+		room1.AddDoor(12, 14, Type.HORIZONTAL);
+		room2.AddDoor(3, 1, Type.HORIZONTAL);
+		room1.name = "room1";
+		room2.name = "room2";
+
+		room1.RenderCanvas();
+		room2.RenderCanvas();
+		List<RoomInstance> initial_rooms = new LinkedList<RoomInstance>();
+		initial_rooms.add(room1);
+		initial_rooms.add(room2);
+		return initial_rooms;
+
+	}
+	
 }
