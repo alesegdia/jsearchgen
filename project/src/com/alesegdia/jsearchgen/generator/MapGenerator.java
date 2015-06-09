910af138335ecd219b0b71b234b3f39cf5eb0c75
Alejandro Seguí <alesegdia@gmail.com>
2015-06-09 23:51:22 +0200

    adding ggm renderer to mapgenerator to watch step by step

--- project/src/com/alesegdia/jsearchgen/generator/MapGenerator.java
+++ project/src/com/alesegdia/jsearchgen/generator/MapGenerator.java
@@ -1,21 +1,27 @@
 package com.alesegdia.jsearchgen.generator;
 
 import com.alesegdia.jsearchgen.model.map.GraphGridModel;
+import com.alesegdia.jsearchgen.view.GraphGridSolutionRenderer;
 
 
 public class MapGenerator {
 
 	protected GraphGridModel ggm;
+	GraphGridSolutionRenderer ggsr;
 
 	MapGenerator(GraphGridModel ggm) {
 		this.ggm = ggm;
+		this.ggsr = new GraphGridSolutionRenderer(ggm);
 	}
 	
 	public void Generate() throws Exception {
+		this.ggsr.Show();
 		while(!this.ggm.IsComplete()){
 			if(!this.Step()) {
 				throw new Exception("ERROR: can't build a complete solution from this partial solution and this list of remaining rooms " + this.ggm);
 			}
+			this.ggsr.Update();
+			//Thread.sleep(1000);
 		}
 	}
 
