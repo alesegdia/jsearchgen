bd3b7855ffeb92079680e3d892663d7b4b4adc4d
Alejandro Seguí <alesegdia@gmail.com>
2015-06-25 20:19:36 +0200

    making mof solver abstract for inheritance usage

--- project/src/com/alesegdia/jsearchgen/fitness/MultiObjectiveFitnessSolver.java
+++ project/src/com/alesegdia/jsearchgen/fitness/MultiObjectiveFitnessSolver.java
@@ -2,31 +2,30 @@ package com.alesegdia.jsearchgen.fitness;
 
 import java.util.List;
 
-import com.alesegdia.jsearchgen.matrixsolver.FloodFillFitnessSolver;
 import com.alesegdia.jsearchgen.matrixsolver.FloydWarshallSolver;
 import com.alesegdia.jsearchgen.util.UpperMatrix2D;
 
-public class MultiObjectiveFitnessSolver implements IFitnessSolver {
+public abstract class MultiObjectiveFitnessSolver implements IFitnessSolver {
 
 	/**
 	 * Longitud del camino principal
 	 */
-	float main_path_length;
+	private float main_path_length;
 	
 	/**
 	 * Número de bifurcaciones (y subbifurcaciones) del camino principal
 	 */
-	float alt_path_branching;
+	private float alt_path_branching;
 	
 	/**
 	 * Número de habitaciones gastadas en caminos alternativos
 	 */
-	float alt_path_length;
+	private float alt_path_length;
 	
 	/**
 	 * Condensación de las habitaciones
 	 */
-	float room_condensation;
+	private float room_condensation;
 
 	private List<Integer> path;
 
@@ -36,18 +35,20 @@ public class MultiObjectiveFitnessSolver implements IFitnessSolver {
 		fws.Solve(new UpperMatrix2D<Float>(graph_matrix));
 		//return fws.GetDistance();
 
-		FloodFillFitnessSolver ffs = new FloodFillFitnessSolver();
+		FloodFillGraphMatrixSolver ffs = new FloodFillGraphMatrixSolver();
 		ffs.Solve(clone, fws.GetPath());
 
 		this.alt_path_branching = ffs.GetBranchingFitness();
-		this.alt_path_length = ffs.GetBranchingFitness();
+		this.alt_path_length = ffs.GetAltPathLengthFitness();
 		this.main_path_length = fws.GetDistance();
 	}
 	
 	@Override
 	public float ComputeFitness(UpperMatrix2D<Float> graph_matrix) {
-		float fitness = this.alt_path_branching + 0.5f * this.alt_path_length;
-		return fitness;
+		ComputeAllFitness(graph_matrix);
+		return ComputeFinalFitness( this.main_path_length, this.alt_path_branching, this.alt_path_length, this.room_condensation );
 	}
 
+	protected abstract float ComputeFinalFitness(float main_path_length_fitness, float alt_path_branching_fitness, float alt_path_length_fitness, float room_condensation_fitness );
+	
 }
