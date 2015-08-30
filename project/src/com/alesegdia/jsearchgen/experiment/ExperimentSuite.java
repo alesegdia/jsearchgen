6c39e61fefdb00c6bbb2bec60d55e870d1d44d40
Alejandro Seguí <alesegdia@gmail.com>
2015-08-30 20:02:00 +0200

    Revert "moving classes used on ExperimentSuite to its own files"
    This reverts commit 5837cd82944573593c473dd1fdd1ec2282509104.

--- project/src/com/alesegdia/jsearchgen/experiment/ExperimentSuite.java
+++ project/src/com/alesegdia/jsearchgen/experiment/ExperimentSuite.java
@@ -1,11 +1,21 @@
 package com.alesegdia.jsearchgen.experiment;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import com.alesegdia.jsearchgen.config.CacheType;
+import com.alesegdia.jsearchgen.config.CombinatorType;
 import com.alesegdia.jsearchgen.config.DoorGenType;
 import com.alesegdia.jsearchgen.config.GenerationConfig;
 import com.alesegdia.jsearchgen.config.ManagerType;
@@ -14,6 +24,7 @@ import com.alesegdia.jsearchgen.machine.DungeonMachine;
 import com.alesegdia.jsearchgen.model.map.TileMap;
 import com.alesegdia.jsearchgen.model.map.TileType;
 import com.alesegdia.jsearchgen.model.room.PrefabManager;
+import com.alesegdia.jsearchgen.model.room.RoomPrefab;
 import com.alesegdia.jsearchgen.util.RNG;
 import com.alesegdia.jsearchgen.util.Vec2;
 
@@ -47,7 +58,141 @@ public class ExperimentSuite {
 		}
 		return pm;
 	}
+	
+	
+	class ExperimentInfoEntry {
+		Vec2 size;
+		int roomsPerType;
+	}
+	
+	class ExperimentResult {
+		float time = 0f;
+	}
 
+	public static class IncompleteGenerationConfig extends GenerationConfig implements Serializable {
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private String name;
+
+		public IncompleteGenerationConfig(String string) {
+			this(0);
+			combinator_type = CombinatorType.PARAMETRIZED;
+			combinator_attack = 0;
+			combinator_decay = 0;
+			fitnesses_params[0] = 1;
+			fitnesses_params[1] = 1;
+			fitnesses_params[2] = 1;
+			fitnesses_params[3] = 1;
+
+			this.name = string;
+		}
+		
+		public IncompleteGenerationConfig(int num_prefabs) {
+			super(num_prefabs);
+		}
+		
+		HashMap<String, String> propsMap = new HashMap<String, String>();
+		
+		public void buildPropsMap() {
+			
+			/* SOLVER TYPE **********************************************/
+			if( this.solver_type == SolverType.BEST_SEARCH ) {
+				propsMap.put("SolverType", "BestSearch");
+				propsMap.put("BestSearch DPE Divisor", String.valueOf(this.bestsearch_dpe_divisor));
+			} else if( this.solver_type == SolverType.RANDOM ) {
+				propsMap.put("SolverType", "Random");
+			} else if( this.solver_type == SolverType.MIXED ) {
+				propsMap.put("SolverType", "Mixed");
+				propsMap.put("BestSearch DPE Divisor", String.valueOf(this.bestsearch_dpe_divisor));
+			}
+			/************************************************************/
+			
+			/* CACHE TYPE ***********************************************/
+			if( this.cache_type == CacheType.NO_CACHE ) {
+				propsMap.put("CacheType", "NO CACHE");
+			} else if( this.cache_type == CacheType.ALWAYS ) {
+				propsMap.put("CacheType", "ALWAYS");
+			} else if( this.cache_type == CacheType.REFRESHER ) {
+				propsMap.put("CacheType", "REFRESHER");
+				propsMap.put("Refresher cache divisor", String.valueOf(this.refresher_n));
+			}
+			/************************************************************/
+
+			if( this.doorgen_type == DoorGenType.ALL ) {
+				propsMap.put("DoorGenType", "ALL");
+			} else if( this.doorgen_type == DoorGenType.RANDOM ) {
+				propsMap.put("DoorGenType", "RANDOM");
+				propsMap.put("Random doors param", ""+this.doorgen_random_doors);
+			} else if( this.doorgen_type == DoorGenType.DIVISOR ) {
+				propsMap.put("DoorGenType", "DIVISOR");
+				propsMap.put("Divisor doors param", "" + this.doorgen_divisor);
+			}
+		}
+		
+		public String toString() {
+			if( propsMap.size() == 0 ) {
+				this.buildPropsMap();
+			}
+			String s = "=============\n" +
+						this.name + "\n";
+			s += "\\begin{center}\n" +
+					   "	\\begin{tabular}{ | c | c | }\n\\hline\n" +
+					   " 		Property & Value \\\\ \\hline\n";
+			for( Map.Entry<String, String> entry : propsMap.entrySet() ) {
+				s += entry.getKey() + " & " + entry.getValue() + " \\\\ \n";
+			}
+			s += "\\hline\n" +
+				 "	\\end{tabular}\n" +
+				 "\\end{center}\n";
+			return s;
+		}
+		
+	}
+	
+	public static class TestCase implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		public IncompleteGenerationConfig cfg;
+		public List<TestEntry> entries = new LinkedList<TestEntry>();
+		
+		public String toString() {
+			String s = cfg.toString() + "\n";
+			s += "\\begin{center}\n" +
+				 "	\\begin{tabular}{ | c | c | c | c | c | }\n\\hline\n";
+
+			s += "Tamaño habitaciones & Número de modelos & Instancias/Modelo & Total habs. & Tiempo de ejecución \\\\ \\hline \n";
+			for( TestEntry te : entries ) {
+				s += te.toString();
+			}
+			
+			s += "\\hline\n" +
+					 "	\\end{tabular}\n" +
+					 "\\end{center}\n";
+					
+			return s;
+		}
+	}
+	
+	public static class TestEntry implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		int size;
+		int numModels;
+		int numRoomsPerModel;
+		float time;
+		
+		public String toString() {
+			return size + " & " + numModels + " & " + numRoomsPerModel + " & " + numModels * numRoomsPerModel + " & " + String.format(Locale.ENGLISH, "%.4f", time) + " \\\\ \n";
+		}
+	}
+	
 	public static IncompleteGenerationConfig MakeDpeDivConfig(String name, float dpediv) {
 		IncompleteGenerationConfig bs_dpediv = new IncompleteGenerationConfig(name);
 		bs_dpediv.solver_type = SolverType.BEST_SEARCH;
