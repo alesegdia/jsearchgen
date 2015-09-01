c25f2b54bb15375d54aa8d02aa75d6c9a7ebac7b
Alejandro Seguí <alesegdia@gmail.com>
2015-09-01 17:42:55 +0200

    adding map samples configs to Test_Machine

--- project/src/com/alesegdia/jsearchgen/test/Test_Machine.java
+++ project/src/com/alesegdia/jsearchgen/test/Test_Machine.java
@@ -21,28 +21,99 @@ public class Test_Machine {
 
 		GenerationConfig gc = new GenerationConfig(pmgr.numPrefabs());
 		gc.num_instances_per_prefab = new int[2];
-		gc.num_instances_per_prefab[0] = 	20;
-		gc.num_instances_per_prefab[1] = 	20;
+		gc.num_instances_per_prefab[0] = 	30;
+		gc.num_instances_per_prefab[1] = 	30;
 		gc.random_seed = 					0xDEADBEEF;
 		gc.cloned_rooms = 					false;
 		gc.doorgen_divisor = 				0.5f;
 		gc.bestsearch_dpe_divisor = 		1f;
-		gc.doorgen_type = 					DoorGenType.ALL;
+		gc.doorgen_type = 					DoorGenType.RANDOM;
+		gc.doorgen_random_doors = 			0.5f;
 		gc.manager_type = 					ManagerType.PREFAB_MODEL;
-		gc.cache_type = 					CacheType.NO_CACHE;
+		gc.cache_type = 					CacheType.REFRESHER;
+		gc.refresher_n = 					5;
 		gc.combinator_type = 				CombinatorType.PARAMETRIZED;
 		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 0f;
 		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 0f;
 		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 100f;
 		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
 		gc.combinator_decay = 				0.5f;
-		gc.combinator_attack = 				1.f;		
+		gc.combinator_attack = 				1.f;
+		gc.bestsearch_dpe_divisor = 		0.9f;
 
 		gc.solver_type = 					SolverType.RANDOM;
 		//LaunchMachineWithParams(gc);
 		
 		gc.solver_type = 					SolverType.BEST_SEARCH;
-		LaunchMachineWithParams(gc);
+		//LaunchMachineWithParams(gc);
+
+		//gc.cache_type = CacheType.NO_CACHE;
+		gc.combinator_type = 				CombinatorType.PARAMETRIZED;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 100f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 0f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 0f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
+		gc.combinator_decay = 				0.5f;
+		gc.combinator_attack = 				1.f;		
+		//LaunchMachineWithParams(gc);
+
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 0f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 100f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 0f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
+		//LaunchMachineWithParams(gc);
+
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 0f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 0f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 100f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
+		//LaunchMachineWithParams(gc);
+
+		// MAINalt
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 0f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 1f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 5.8f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
+		//LaunchMachineWithParams(gc);
+
+		// MAINaltbranch
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 1f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 1f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 5.8f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
+		//LaunchMachineWithParams(gc);
+
+		// adaptMAINaltbranch
+		gc.combinator_type = 				CombinatorType.ADAPTATIVE_PARAMETRIZED;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 1f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 1f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 10f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
+		gc.combinator_decay = 				0.5f;
+		gc.combinator_attack = 				1.f;
+		//LaunchMachineWithParams(gc);
+
+		// adaptmainaltbranch
+		gc.combinator_type = 				CombinatorType.ADAPTATIVE_PARAMETRIZED;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 10f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 10f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 10f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
+		gc.combinator_decay = 				0.5f;
+		gc.combinator_attack = 				1f;
+		//LaunchMachineWithParams(gc);
+
+		// adaptmainaltbranch2
+		gc.combinator_type = 				CombinatorType.ADAPTATIVE_PARAMETRIZED;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_BRANCHING] = 10f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ALT_PATH_LENGTH] = 10f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_MAIN_PATH_LENGTH] = 10f;
+		gc.fitnesses_params[MultiObjectiveFitness.FO_ROOM_CONDENSATION] = 0f;
+		gc.combinator_decay = 				0.9f;
+		gc.combinator_attack = 				1.1f;
+		//LaunchMachineWithParams(gc);
+		
+
 	}
 	
 	static void LaunchMachineWithParams(GenerationConfig gc) throws Exception {
